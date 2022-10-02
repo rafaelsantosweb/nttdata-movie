0bc1fe19e292598f954fcd2101d5977e35effeb5
rafaelsantosweb <rafaelsantosweb@gmail.com>
2022-10-02 15:50:50 -0300

    NTTDATA test drupal 9

---
+++ drupal/web/sites/default/files/js/js_AVU7RgrPTnynSU0QhLe5gOMxD2DVRfX8l-8ehE0gyuk.js
@@ -0,0 +1,18 @@
+/**
+ * @file
+ * nttdata behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.nttdata = {
+    attach: function (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (Drupal));
+;
